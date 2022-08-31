f9aa213a065ef4a2ab17e8beff8e554c3b02b61f
AdamLars <78119054+AdamLars@users.noreply.github.com>
2022-08-31 11:12:37 -0700

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -7,12 +7,12 @@ Adam Larson and Madalyn Henderson
 problem = [[1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 0, 0, 0, 1, 0, 0, 0, 1], [1, 0, 0, 0, 1, 0, 0, 'R', 1], [1, 0, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 1, 0, 0, 1], [1, 1, 1, 0, 0, 1, 0, 0, 1], [1, 1, 1, 0, 0, 1, 1, 1, 1], [1, 1, 0, 0, 0, 0, 0, 'D', 1], [1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
 #goalTest
-#create fringe
 #successor function
 #save plan and return solution
 #ask user for map to play
 #calculate cost
 
+#Defining the tree node
 class treeNode:
   def __init__(l, c, p):
     location = l
@@ -41,7 +41,33 @@ def findStartState(problem):
                 startState.append(y)
                 return startState
 
+#define goal state
+def findGoalState(problem):
+    #go to 'G' in problem
+    #return location of G
+    goalState = []
+    x = 0
+    y = 0
+    for i in range(0,len(problem),1):
+        for j in range(0,len(problem[i]),1):
+            if(problem[i][j] == 'G'):
+                x = i
+                y = j
+                goalState.append(x)
+                goalState.append(y)
+                return goalState
+
 def goalTest(currentNode):
   currentNode.location
 
 currentNode = treeNode(findStartState(problem), 0)
+#Create fringe and add current node
+fringe = []
+fringe.append(currentNode)
+for i in 1000000:
+  currentNode = fringe[0]
+  while(problem[currentNode[0].location[0]-1][currentNode[0].location[1]] != 1):
+    currentNode.updateParent(currentNode)
+    currentLocate = [currentNode[0].location[0]-1, currentNode[0].location[1]]
+    currentNode.updateLocation(currentLocate)
+    currentNode.updateCost(currentNode.cost + 1)
