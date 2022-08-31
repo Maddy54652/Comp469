e3c33214d5747dc6fc0f3120fcbd4cd16b9b1a20
Maddy <37761862+Maddy54652@users.noreply.github.com>
2022-08-31 10:21:00 -0700

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,19 @@
+'''
+Lab 2
+Adam Larson and Madalyn Henderson
+8/31/22
+'''
+#defining the problem that we will pass through
+problem = [[1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 0, 0, 0, 1, 0, 0, 0, 1], [1, 0, 0, 0, 1, 0, 0, R, 1] [1, 0, 0, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 1, 0, 0, 1], [1, 1, 1, 0, 0, 1, 0, 0, 1], [1, 1, 1, 0, 0, 1, 1, 1, 1], [1, 1, 0, 0, 0, 0, 0, D, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+#define start state
+#goalTest
+#create fringe
+#successor function
+#save plan and return solution
+#ask user for map to play
+#calculate cost
+
+def findStartState(problem):
+    #go to 'R' in problem
+    #return location of R
